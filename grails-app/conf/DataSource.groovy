40407e9d8c0ada55609e0e34061cd78e9394ff32
Vladimir <vladimir.shulgin@gmail.com>
2013-10-31 20:17:49 +0100

    Added Heroku dataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,12 +1,14 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "org.postgresql.Driver"
+	dialect = org.hibernate.dialect.PostgreSQLDialect
+    username = "mutlscurujleoz"
+    password = "3VJ9yFjOvdJRY2T3ZMdFnvBD64"
+	
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
 //    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
@@ -15,8 +17,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://ec2-54-235-174-213.compute-1.amazonaws.com:5432/dc60kdeggqi3rm"
         }
     }
     test {
@@ -28,7 +30,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:postgresql://ec2-54-235-174-213.compute-1.amazonaws.com:5432/dc60kdeggqi3rm"
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
